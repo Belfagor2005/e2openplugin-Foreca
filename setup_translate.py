66a75feb71cae5f9c0d30d7f0a496a8cb34db32d
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-03-02 10:04:55 +0100

    v.3.3.8

--- setup_translate.py
+++ setup_translate.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+
 # Language extension for distutils Python scripts. Based on this concept:
 # http://wiki.maemo.org/Internationalize_a_Python_application
 from distutils import cmd
